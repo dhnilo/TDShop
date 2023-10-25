577cede49bb430036f8d16a9d806943cb0851a8a
Drew Hnilo <dhnilo.dev@gmail.com>
2023-10-24 23:54:04 -0500

    Backend Authentication implemented

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -23,6 +24,22 @@ const userSchema = mongoose.Schema(
   }, {
     timestamps: true,
   });
+
+//Checks if password matches the hashed password in the database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//Hashes the password before saving the user to the database
+userSchema.pre("save", async function (next) {
+  //Only hash the password if it is modified or new
+  if (!this.isModified("password")) {
+    next();
+  }
+  //Salt is a random string added to the password before hashing to make it more secure
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 //Pulling port from .env file
@@ -12,12 +14,20 @@ connectDB(); //Connecting to MongoDB
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 //Home route
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
