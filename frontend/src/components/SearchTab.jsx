07fab35587537666fb9e304fb4d74abda99b9952
Thaddeus-Bielecki <twb5147@truman.edu>
2023-11-07 14:23:13 -0600

    added the transition functionality to the search button

--- frontend/src/components/SearchTab.jsx
+++ frontend/src/components/SearchTab.jsx
@@ -29,7 +29,7 @@ const SearchTab = () => {
         placeholder='Search Products...'
         className='mr-sm-2 ml-sm-5'
       ></Form.Control>
-      <Button type='submit' variant='outline-success' className='p-2 mx-2'>
+      <Button type='submit' className='fancyButton'>
         Search
       </Button>
     </Form>
